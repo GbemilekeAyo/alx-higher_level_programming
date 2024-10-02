207f1d359d0497a0b25bbab5209ef4b3e4658397
Gbemileke Ayodele <onabajogbemileke37@gmail.com>
2024-10-02 15:10:17 -0700

    completed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
+    count = 0
+
+    for i in range(0, x):
         try:
-            print(my_list[m], end="")
-            n += 1
-        except IndexError:
-            break
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except:
+            pass
     print()
-    return n
+    return count
